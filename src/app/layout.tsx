41e328c95b87351161fdb2962a51a210ce272473
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-02-03 15:33:25 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>MedicalSuite - Abraham Aracena</title>
       {/* <link rel="manifest" href="manifest.json" /> */}
       </head>
       <body suppressHydrationWarning={true}>
