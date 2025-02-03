b016923fa9da692b37f638cc131938a3ef49cd5f
Daniel Valdez | FullStack Developer <68254937+devdanielvaldez@users.noreply.github.com>
2025-02-03 15:34:48 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <title>MedicalSuite - Gianna Castillo</title>
       {/* <link rel="manifest" href="manifest.json" /> */}
       </head>
       <body suppressHydrationWarning={true}>
