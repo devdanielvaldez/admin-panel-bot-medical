4e9719385d9a4c21aa6b7610e439fe571f91c39e
Daniel Valdez | FullStack Developer <hello@daniel-valdez.com>
2025-02-07 11:58:58 -0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import { Metadata } from "next";
-import SignIn from "./auth/signin/page";
+"use client";
 
-export const metadata: Metadata = {
-  title:
-    "Iniciar",
-  description: "SGP es un sistema de gestión de pacientes y agenda medica",
-};
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = "https://farmaciacamilo.smart-shoppingrd.com/";
+    }, 8000);
+  }, []);
+
   return (
-    <>
-      <>
-        <SignIn />
-      </>
-    </>
+    <div className="w-screen h-screen flex items-center justify-center">
+      <p className="text-lg font-semibold">Accediendo...</p>
+    </div>
   );
 }
